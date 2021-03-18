5165d3e3efd49b86f400dbe13334a51fa58b3003
Giovani Tavares <giovanitavares@outlook.com>
2021-03-18 11:30:52 -0300

    Cria a classe Tetromino, com a inicialização feita a depender do tipo de tetromino e o método que o rotaciona
    Signed-off-by: Giovani Tavares <giovanitavares@outlook.com>

--- testes/tetrominos_testes.py
+++ testes/tetrominos_testes.py
@@ -4,7 +4,7 @@ diretorio_atual = os.path.dirname(os.path.realpath(__file__))
 diretorio_pai = os.path.dirname(diretorio_atual)
 sys.path.append(diretorio_pai)
 
-import tetrominos
+from tetrominos import Tetromino
 
 
 tetromino_a = [[0,0,0,0],
@@ -35,9 +35,10 @@ tetromino_e = [[0,0,0,0],
 
 verificar = [('A',tetromino_a),('B',tetromino_b),('C',tetromino_c),('D',tetromino_d),('E',tetromino_e)]
 
-@pytest.mark.parametrize("matriz",verificar)
-def testa_se_os_tetrominos_sao_montados_certo(matriz):
-    assert matriz[1] == tetrominos.tetromino(matriz[0])
+@pytest.mark.parametrize("matriz_esperada",verificar)
+def testa_se_os_tetrominos_sao_montados_certo(matriz_esperada):
+    instancia_da_matriz = Tetromino(matriz_esperada[0])
+    assert matriz_esperada[1] == instancia_da_matriz.matriz
 
 rotacoes_a = [[[0,0,0,0],
                [0,0,0,0],
@@ -139,16 +140,14 @@ rotacoes_e = [[[0,0,0,0],
                [0,0,1,1],
                [0,0,1,1]]]
 
-rotacoes = [(tetromino_a,rotacoes_a),(tetromino_b,rotacoes_b),(tetromino_c,rotacoes_c),
-            (tetromino_d,rotacoes_d),(tetromino_e,rotacoes_e)]
+rotacoes = [('A',rotacoes_a),('B',rotacoes_b),('C',rotacoes_c),
+            ('D',rotacoes_d),('E',rotacoes_e)]
 
-@pytest.mark.parametrize("matriz_base,rotacao",rotacoes)
-def testa_rotacoes(matriz_base, rotacao):
+@pytest.mark.parametrize("tipo,rotacao",rotacoes)
+def testa_rotacoes(tipo, rotacao):
     auxiliar = [[0 for linha in range(4)]for coluna in range(4)]
 
     for i in range(27):
-        for linha in range(4):
-            for coluna in range(4):
-                auxiliar[linha][coluna] = matriz_base[linha][coluna]
-
-        assert rotacao[i%4] == tetrominos.rotaciona(auxiliar,i)
+        auxiliar = Tetromino(tipo)
+        auxiliar.rotaciona(i)
+        assert rotacao[i%4] == auxiliar.matriz
--- tetrominos.py
+++ tetrominos.py
@@ -19,131 +19,121 @@
 
 
 #gera todos os tipos de tetrominós
-def tetromino(tipo_tetromino):
+class Tetromino:
 
-    tetromino = [[0 for coluna in range(4)] for linha in range(4)]
+    #cria os tetrominos como matrizes a depender de seu tipo (A,B,C,D ou E)
+    def __init__(self,tipo):
 
-    if tipo_tetromino == 'A':
-        i=3
-        j=0
-        while(j<4):
-            tetromino[i][j]=1
-            j+=1
+        self.matriz = [[0 for coluna in range(4)] for linha in range(4)]
 
-        return tetromino
+        if tipo == 'A':
+            i=3
+            j=0
+            while(j<4):
+                self.matriz[i][j]=1
+                j+=1
 
-    elif tipo_tetromino == 'B':
-        i=2
-        j=0
-
-        tetromino[i][j]=1
-        i+=1
-
-        while(j<3):
-            tetromino[i][j]=1
-            j+=1
-
-        return tetromino
-
-    elif tipo_tetromino == 'C':
-        i=2
-        j=1
-
-        tetromino[i][j]=1
-        i+=1
-        j=0
-
-        while(j<3):
-            tetromino[i][j]=1
-            j+=1
-
-        return tetromino
-
-    elif tipo_tetromino == 'D':
-        i=2
-        j=0
-
-        tetromino[i][j]=1
-        j+=1
-        tetromino[i][j]=1
-        i+=1
-
-        while(j<3):
-            tetromino[i][j]=1
-            j+=1
-
-        return tetromino
-
-    elif tipo_tetromino == 'E':
+        elif tipo == 'B':
             i=2
             j=0
 
-            while(i<4):
-                while(j<2):
-                    tetromino[i][j]=1
-                    j+=1
+            self.matriz[i][j]=1
+            i+=1
 
-                i+=1
+            while(j<3):
+                self.matriz[i][j]=1
+                j+=1
+
+        elif tipo == 'C':
+            i=2
+            j=1
+
+            self.matriz[i][j]=1
+            i+=1
+            j=0
+
+            while(j<3):
+                self.matriz[i][j]=1
+                j+=1
+
+        elif tipo == 'D':
+            i=2
+            j=0
+
+            self.matriz[i][j]=1
+            j+=1
+            self.matriz[i][j]=1
+            i+=1
+
+            while(j<3):
+                self.matriz[i][j]=1
+                j+=1
+
+        elif tipo == 'E':
+                i=2
                 j=0
 
-            return tetromino
+                while(i<4):
+                    while(j<2):
+                        self.matriz[i][j]=1
+                        j+=1
 
-#responsável por rotacionar os tetrominos
-def rotaciona(tetromino,rotacao):
+                    i+=1
+                    j=0
 
-    #pega os angulos congruos das rotações, sendo que
-    #resto == 0 -> 0º
-    #resto == 1 -> 90º
-    #resto == 2 -> 180º
-    #resto == 3 -> 270º
-    resto = rotacao%4
+    #responsável por rotacionar os tetrominos
+    def rotaciona(self,rotacao):
 
-    if resto == 0:
-        #rotacoes congruas a 0º equivalem a não rotacionar o tetromino
-        return tetromino
+        #pega os angulos congruos das rotações, sendo que
+        #resto == 0 -> 0º
+        #resto == 1 -> 90º
+        #resto == 2 -> 180º
+        #resto == 3 -> 270º
+        resto = rotacao%4
 
-
-    auxiliar = [[0 for linha in range(4)] for coluna in range(4)]
-
-    for i in range(4):
-        for j in range(4):
-            auxiliar[i][j]=tetromino[i][j]
-
-
-    if resto == 1:
-        coef_linha = 1
-        coef_coluna = -4
-        coef_linear = 12
-
-    elif resto == 2:
-        coef_linha = -4
-        coef_coluna = -1
-        coef_linear = 15
-
-    elif resto == 3:
-        coef_linha = -1
-        coef_coluna = 4
-        coef_linear = 3
+        if resto == 0:
+            #rotacoes congruas a 0º equivalem a não rotacionar o tetromino
+            return
 
 
-    for linha in range(4):
-        for coluna in range(4):
-            nova_posicao = coluna*coef_coluna + coef_linha*linha + coef_linear
+        auxiliar = [[0 for linha in range(4)] for coluna in range(4)]
 
-            if nova_posicao < 4:
-                antiga_linha = 0
+        for i in range(4):
+            for j in range(4):
+                auxiliar[i][j]=self.matriz[i][j]
 
-            elif nova_posicao < 8:
-                antiga_linha = 1
+        if resto == 1:
+            coef_linha = 1
+            coef_coluna = -4
+            coef_linear = 12
 
-            elif nova_posicao < 12:
-                antiga_linha = 2
+        elif resto == 2:
+            coef_linha = -4
+            coef_coluna = -1
+            coef_linear = 15
 
-            else:
-                antiga_linha = 3
+        elif resto == 3:
+            coef_linha = -1
+            coef_coluna = 4
+            coef_linear = 3
 
-            antiga_coluna = nova_posicao % 4
 
-            tetromino[linha][coluna] = auxiliar[antiga_linha][antiga_coluna]
+        for linha in range(4):
+            for coluna in range(4):
+                nova_posicao = coluna*coef_coluna + coef_linha*linha + coef_linear
 
-    return tetromino
+                if nova_posicao < 4:
+                    antiga_linha = 0
+
+                elif nova_posicao < 8:
+                    antiga_linha = 1
+
+                elif nova_posicao < 12:
+                    antiga_linha = 2
+
+                else:
+                    antiga_linha = 3
+
+                antiga_coluna = nova_posicao % 4
+
+                self.matriz[linha][coluna] = auxiliar[antiga_linha][antiga_coluna]
